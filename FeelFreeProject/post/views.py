a8964a9ce51e0e1a56ed4e82f095d6d608f99b85
likelionTeamOne <likelionteamone@gmail.com>
2024-06-25 23:41:23 +0900

    comment e

--- FeelFreeProject/post/views.py
+++ FeelFreeProject/post/views.py
@@ -10,6 +10,9 @@ class CommentViewSet(ModelViewSet) :
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
+    def perform_create(self, serializer) :
+        serializer.save(writer = self.request.user)
+
     def get_queryset(self, **kwargs):
         id = self.kwargs['post_id']
         return self.queryset.filter(post=id)
