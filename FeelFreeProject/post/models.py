a97ab35d2435e7e02b7273e90fdefae78253c1fa
likelionTeamOne <likelionteamone@gmail.com>
2024-06-25 15:45:45 +0900

    add post/models

--- FeelFreeProject/post/models.py
+++ FeelFreeProject/post/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from django.db import models # type: ignore
 
-# Create your models here.
+# 게시글 모델
+class Post(models.Model) : 
+    title = models.CharField(verbose_name="제목", max_length=128)
+
+    def __str__(self):
+        return self.title
+
+# 댓글 모델
+class Comment(models.Model) :
+    comment = models.CharField(verbose_name="댓글", max_length=128)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self) :
+        return self.comment
